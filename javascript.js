ed62ff752c38e41054db7d1eb432922d19c9eade
ismailas <ismail.a.shobar@gmail.com>
2016-12-14 22:48:30 +0700

    uplod

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var cw = $('.arrow2').width();
+$('.arrow2').css({
+    'height': 40 + 'px'
+});
+
+// window.addEventListener("resize", function(e) {
+//     var mapElement = document.getElementById("pp");
+//     mapElement.style.height = mapElement.offsetWidth ;
+// });
